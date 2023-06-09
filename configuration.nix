23af7e3d31d9f03f2ce6b3b3e8e18a45a559364f
muiga <muigask@gmail.com>
2023-06-09 12:29:39 +0300

    init

---
+++ configuration.nix
@@ -0,0 +1,352 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+      <home-manager/nixos>
+    ];
+
+  # Bootloader.
+  boot ={
+    kernelPackages = pkgs.linuxPackages_latest;
+    initrd.kernelModules = [ "amdgpu" ];       # Video drivers
+
+    loader = {
+      systemd-boot = {
+        enable =true;
+        configurationLimit = 5;
+      };
+      efi.canTouchEfiVariables = true;
+      timeout = 1;
+    };
+  };
+
+  systemd.extraConfig = ''
+    DefaultTimeoutStopSec=5s
+  '';
+
+  # Setup keyfile
+  boot.initrd.secrets = {
+    "/crypto_keyfile.bin" = null;
+  };
+
+  networking.hostName = "nixos"; # Define your hostname.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Enable networking
+  networking.networkmanager.enable = true;
+
+  # Set your time zone.
+  time.timeZone = "Africa/Nairobi";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  # Enable the X11 windowing system.
+  services.xserver = {
+	  enable = true;
+	  layout = "us";
+	  # xkbVariant = "";
+    displayManager = {
+	   sddm.enable = true;
+  	  defaultSession = "plasmawayland";
+    };
+    desktopManager.plasma5.enable = true;	
+  };
+
+  # Enable the KDE Plasma Desktop Environment.
+  # services.xserver.displayManager = {
+	#   sddm.enable = true;
+  # 	defaultSession = "plasmawayland";
+  # };	
+  # services.xserver.desktopManager.plasma5.enable = true;
+
+  # Configure keymap in X11
+  #services.xserver = {
+  #  layout = "us";
+  #  xkbVariant = "";
+  #};
+
+  
+  # services.printing.enable = true;
+
+  #All Services
+  services = {
+  # Enable CUPS to print documents.
+    printing = {                                # Printing and drivers for TS5300
+      enable = true;
+      #drivers = [ pkgs.cnijfilter2 ];          # There is the possibility cups will complain about missing cmdtocanonij3. I guess this is just an error that can be ignored for now. Also no longer need required since server uses ipp to share printer over network.
+    };
+    avahi = {                                   # Needed to find wireless printer
+      enable = true;
+      nssmdns = true;
+      publish = {                               # Needed for detecting the scanner
+        enable = true;
+        addresses = true;
+        userServices = true;
+      };
+    };
+    pipewire = {                            # Sound
+      enable = true;
+      alsa = {
+        enable = true;
+        support32Bit = true;
+      };
+      pulse.enable = true;
+      jack.enable = true;
+    };
+
+  # Enable the OpenSSH daemon.
+    openssh = {                             # SSH: secure shell (remote connection to shell of server)
+      enable = true;                        # local: $ ssh <user>@<ip>
+                                            # public:
+                                            #   - port forward 22 TCP to server
+                                            #   - in case you want to use the domain name insted of the ip:
+                                            #       - for me, via cloudflare, create an A record with name "ssh" to the correct ip without proxy
+                                            #   - connect via ssh <user>@<ip or ssh.domain>
+                                            # generating a key:
+                                            #   - $ ssh-keygen   |  ssh-copy-id <ip/domain>  |  ssh-add
+                                            #   - if ssh-add does not work: $ eval `ssh-agent -s`
+      allowSFTP = true;                     # SFTP: secure file transfer protocol (send file to server)
+                                            # connect: $ sftp <user>@<ip/domain>
+                                            #   or with file browser: sftp://<ip address>
+                                            # commands:
+                                            #   - lpwd & pwd = print (local) parent working directory
+                                            #   - put/get <filename> = send or receive file
+      extraConfig = ''
+        HostKeyAlgorithms +ssh-rsa
+      '';                                   # Temporary extra config so ssh will work in guacamole
+    };
+  # Enable flatpak
+    flatpak.enable = true;                  # download flatpak file from website - sudo flatpak install <path> - reboot if not showing up
+                                            # sudo flatpak uninstall --delete-data <app-id> (> flatpak list --app) - flatpak uninstall --unused
+                                            # List:
+                                            # com.obsproject.Studio
+                                            # com.parsecgaming.parsec
+                                            # com.usebottles.bottles
+  };
+
+  # Enable sound with pipewire.
+  sound.enable = true;
+  hardware.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  security.polkit.enable = true;
+  # services.pipewire = {
+  #   enable = true;
+  #   alsa.enable = true;
+  #   alsa.support32Bit = true;
+  #   pulse.enable = true;
+  #   # If you want to use JACK applications, uncomment this
+  #   #jack.enable = true;
+
+  #   # use the example session manager (no others are packaged yet so this is enabled by default,
+  #   # no need to redefine it in your config for now)
+  #   #media-session.enable = true;
+  # };
+
+  # Enable touchpad support (enabled default in most desktopManager).
+  # services.xserver.libinput.enable = true;
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.muiga = {
+    isNormalUser = true;
+    description = "Stephen M Kamau";
+    extraGroups = [ "networkmanager" "wheel" ];
+    shell = pkgs.zsh;
+    packages = with pkgs; [
+      firefox
+      kate
+      vscode
+      vlc
+      brave
+      git
+      curl
+      thunderbird
+      latte-dock
+      gimp-with-plugins
+      inkscape-with-extensions
+      postman
+      haruna
+      discord
+      motrix
+      persepolis
+      lightly-qt
+      sierra-breeze-enhanced
+      pcloud
+      obs-studio
+      easyeffects
+    ];
+
+    
+  };
+
+  # zsh and ohmyzsh
+  programs = {
+    zsh = {
+      enable = true;
+    };
+  };
+
+  #Fonts
+   fonts.fonts = with pkgs; [                # Fonts
+    carlito                                 # NixOS
+    vegur                                   # NixOS
+    source-code-pro
+    jetbrains-mono
+    font-awesome                            # Icons
+    corefonts                               # MS
+    (nerdfonts.override {                   # Nerdfont Icons override
+      fonts = [
+        "FiraCode"
+      ];
+    })
+  ];
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  #environment.systemPackages = with pkgs; [
+  #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+  #  wget
+  #];
+  environment = {
+    systemPackages = with pkgs.libsForQt5;[
+      packagekit-qt
+      bismuth
+      kdenlive
+      sddm-kcm
+      kdeconnect-kde	
+    ];
+    shells = with pkgs; [
+      zsh
+    ];
+  };
+
+
+  #enable bluetooth
+  hardware.bluetooth.enable = true;
+  services.blueman.enable = true;
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  # services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  # system.stateVersion = "23.05"; # Did you read the comment?
+
+  # home-manager.useGlobalPkgs = true;
+  # home-manager.users.muiga = { pkgs, ... }: {
+  #  home.packages = [ pkgs.atool pkgs.httpie ];
+  #   programs = {
+  #    zsh = {
+  #       enable = true;
+  #       # autosuggestions.enable = true;            # Auto suggest options and highlights syntax, searches in history for options
+  #       # syntaxHighlighting.enable = true;
+  #       # enableCompletion = true;
+  #       # histSize = 100000;
+
+  #       # ohMyZsh = {                               # Extra plugins for zsh
+  #       #   enable = true;
+  #       #   plugins = [ "git" ];
+  #       # };
+
+  #       # shellInit = ''                            # Zsh theme
+  #       #   # Spaceship
+  #       #   source ${pkgs.spaceship-prompt}/share/zsh/site-functions/prompt_spaceship_setup
+  #       #   autoload -U promptinit; promptinit
+  #       #   # Hook direnv
+  #       #   #emulate zsh -c "$(direnv hook zsh)"
+  #       #   # Swag
+  #       #   ${pkgs.nitch}/bin/nitch
+
+  #       #   #eval "$(direnv hook zsh)"
+  #       # '';
+  #     };
+  #   };
+  # };
+
+
+   home-manager.users.muiga = { pkgs, ... }: {
+   home.packages = [ pkgs.atool pkgs.httpie ];
+    # programs = {
+    #  zsh = {
+    #     enable = true;
+    #     # autosuggestions.enable = true;            # Auto suggest options and highlights syntax, searches in history for options
+    #     # syntaxHighlighting.enable = true;
+    #     # enableCompletion = true;
+    #     # histSize = 100000;
+
+    #     # ohMyZsh = {                               # Extra plugins for zsh
+    #     #   enable = true;
+    #     #   plugins = [ "git" ];
+    #     # };
+
+    #     # shellInit = ''                            # Zsh theme
+    #     #   # Spaceship
+    #     #   source ${pkgs.spaceship-prompt}/share/zsh/site-functions/prompt_spaceship_setup
+    #     #   autoload -U promptinit; promptinit
+    #     #   # Hook direnv
+    #     #   #emulate zsh -c "$(direnv hook zsh)"
+    #     #   # Swag
+    #     #   ${pkgs.nitch}/bin/nitch
+
+    #     #   #eval "$(direnv hook zsh)"
+    #     # '';
+    #   };
+    # };
+  };
+
+
+  nix = {                                   # Nix Package Manager settings
+    settings ={
+      auto-optimise-store = true;           # Optimise syslinks
+    };
+    gc = {                                  # Automatic garbage collection
+      automatic = true;
+      dates = "weekly";
+      options = "--delete-older-than 2d";
+    };
+   
+  };
+
+  system = {                                # NixOS settings
+    autoUpgrade = {                         # Allow auto update (not useful in flakes)
+      enable = true;
+      channel = "https://nixos.org/channels/nixos-unstable";
+    };
+    stateVersion = "23.05";
+  };
+
+
+}
